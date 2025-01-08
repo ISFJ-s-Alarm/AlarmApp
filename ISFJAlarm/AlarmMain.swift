9962cc706bb25f8925dd5c1404925589dafee329
Quaker-Lee <tkacl2010@gmail.com>
2025-01-09 01:58:58 +0900

    Feat: 코드 리팩토링, NavigationBar 버튼 클릭시 모달 띄우는 기능 구현

--- ISFJAlarm/AlarmMain.swift
+++ ISFJAlarm/AlarmMain.swift
@@ -30,16 +30,30 @@ class ViewController: UIViewController {
     
     //MARK: navigationBar
     private func navigationBar() {
-        //생성
-        let navigationBar = UINavigationBar(frame: CGRect(x: 0, y: 0, width: view.frame.width, height: 44))
-        
+        //TIL
         //let navigationItem = UINavigationItem(title:"home")
-        let addBtn = UIBarButtonItem(title: "+", style: .plain, target: self, action: #selector(addBtnTapped))
-        navigationItem.rightBarButtonItem = addBtn
+        //self.title = "Home"
         
+        //편집버튼 설정
         let editBtn = UIBarButtonItem(title: "편집", style: .plain, target: self, action: #selector(editBtnTapped))
+        // 타이틀 폰트 크기 및 색상 설정
+        let leftAttributes: [NSAttributedString.Key: Any] = [
+            .font: UIFont.systemFont(ofSize: 20), // 폰트 크기 설정
+            .foregroundColor: UIColor.green         // 텍스트 색상 설정
+        ]
+        editBtn.setTitleTextAttributes(leftAttributes, for: .normal)
         navigationItem.leftBarButtonItem = editBtn
-        navigationBar.setItems([navigationItem], animated: false)
+        
+        //+버튼 설정
+        let addBtn = UIBarButtonItem(title: "+", style: .plain, target: self, action: #selector(addBtnTapped))
+        // 타이틀 폰트 크기 및 색상 설정
+        let rightAttributes: [NSAttributedString.Key: Any] = [
+            .font: UIFont.systemFont(ofSize: 30), // 폰트 크기 설정
+            .foregroundColor: UIColor.green         // 텍스트 색상 설정
+        ]
+        addBtn.setTitleTextAttributes(rightAttributes, for: .normal)
+        navigationItem.rightBarButtonItem = addBtn
+    
     }
     
     //MARK: UI 정의
@@ -58,10 +72,10 @@ class ViewController: UIViewController {
     //MARK: Button Actions
     @objc
     private func addBtnTapped() {
-        
+        present(ModalTestVC(), animated: true, completion: nil)
     }
     @objc func editBtnTapped() {
-        
+        present(ModalTestVC(), animated: true, completion: nil)
     }
 
 }
--- ISFJAlarm/ModalTestVC.swift
+++ ISFJAlarm/ModalTestVC.swift
@@ -12,9 +12,21 @@ import Then
 
 class ModalTestVC: ViewController {
     
+    let label = UILabel().then {
+        $0.text = "모달 띄우기 성공"
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        configureUI()
     }
     
+    private func configureUI() {
+        view.backgroundColor = .white
+        
+        view.addSubview(label)
+        label.snp.makeConstraints {
+            $0.center.equalToSuperview()
+        }
+    }
 }
