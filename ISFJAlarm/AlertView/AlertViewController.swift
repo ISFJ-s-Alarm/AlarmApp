e9ca145c5a89745d6f2f6c20aa72a64cd4fc0026
solnamul <sol7113@gmail.com>
2025-01-09 22:00:33 +0900

    feat: 현재 시각 기능 구현 (시,분,초)

--- ISFJAlarm/AlertView/AlertViewController.swift
+++ ISFJAlarm/AlertView/AlertViewController.swift
@@ -10,15 +10,23 @@ import AVKit
 
 class AlertViewController: UIViewController {
     var player: AVPlayer?
+    private let timeLabel = UILabel() // 현재 시간 표시를 위한 UILabel
+    private var timer: Timer? // 시간을 갱신하기 위한 타이머
 
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .black // 동영상이 나오기 전 검정 배경 화면 설정
         setupVideoBackground()
+        setupTimeLabel() // 시간 라벨 설정!
         setupSnoozeButton()
         setupDismissButton()
     }
 
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+        timer?.invalidate() // 화면이 사라질 때 타이머 종료
+    }
+    
     // 배경 동영상 설정 (에셋에서 불러오기 + 반복 재생)
     func setupVideoBackground() {
         guard let asset = NSDataAsset(name: "morning1") else {
@@ -51,6 +59,36 @@ class AlertViewController: UIViewController {
         player?.seek(to: .zero)
         player?.play()
     }
+    
+    // 현재 시간 표시를 위한 UILabel 설정
+        func setupTimeLabel() {
+            timeLabel.textColor = .white
+            timeLabel.font = UIFont.systemFont(ofSize: 32, weight: .bold)
+            timeLabel.textAlignment = .center
+            timeLabel.translatesAutoresizingMaskIntoConstraints = false
+            view.addSubview(timeLabel)
+
+            NSLayoutConstraint.activate([
+                timeLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+                timeLabel.centerYAnchor.constraint(equalTo: view.centerYAnchor, constant: -200) // 화면 중앙보다 위로 100포인트
+            ])
+
+            // 타이머 시작
+            startTimer()
+        }
+
+        // 타이머 시작
+        func startTimer() {
+            updateTime() // 초기 시간 설정
+            timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updateTime), userInfo: nil, repeats: true)
+        }
+
+        // 현재 시간을 업데이트
+        @objc func updateTime() {
+            let formatter = DateFormatter()
+            formatter.dateFormat = "HH:mm:ss" // 시:분:초 형식
+            timeLabel.text = formatter.string(from: Date())
+        }
 
     // "다시 알림" 버튼 추가!
     func setupSnoozeButton() {
