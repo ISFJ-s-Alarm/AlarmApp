58713e7148fb0ac497e7b7d5653fc4bf607cb87d
“hyunwook-sevo” <seojy1276@naver.com>
2025-01-14 21:26:29 +0900

    fix: 알람 메인 뷰 네비게이션바가 표시되지 않던 문제 수정

--- ISFJAlarm/CustomTabBar/ViewController/CustomTabBarViewController.swift
+++ ISFJAlarm/CustomTabBar/ViewController/CustomTabBarViewController.swift
@@ -10,26 +10,26 @@ import SnapKit
 /// 커스텀 탭바 컨트롤러
 class CustomTabBarController: UIViewController {
     private let tabBarView = CustomTabBarView() // 커스텀 탭 바
-
+    
     // 화면별 뷰 컨트롤러
-    private let timerVC = TimerView()
-    private let stopwatchVC = StopwatchViewController()
-    private let alarmVC = ViewController()
-
+    private let timerVC = UINavigationController(rootViewController: TimerView())
+    private let stopwatchVC = UINavigationController(rootViewController: StopwatchViewController())
+    private let alarmVC = UINavigationController(rootViewController: ViewController())
+    
     // 현재 활성화된 뷰 컨트롤러
     private var currentViewController: UIViewController?
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         setupUI()
         setupTabBarActions()
         switchToViewController(alarmVC) // 초기 화면 설정
     }
-
+    
     /// UI 구성
     private func setupUI() {
         view.backgroundColor = .white
-
+        
         // 커스텀 탭바 추가
         view.addSubview(tabBarView)
         tabBarView.snp.makeConstraints {
@@ -38,7 +38,7 @@ class CustomTabBarController: UIViewController {
             $0.height.equalTo(80)
         }
     }
-
+    
     /// 탭바 버튼 동작 설정
     private func setupTabBarActions() {
         tabBarView.onTabSelected = { [weak self] index in
@@ -51,7 +51,7 @@ class CustomTabBarController: UIViewController {
             }
         }
     }
-
+    
     /// 현재 뷰 컨트롤러 전환
     /// - Parameter newViewController: 새로 활성화할 뷰 컨트롤러
     private func switchToViewController(_ newViewController: UIViewController) {
@@ -59,7 +59,7 @@ class CustomTabBarController: UIViewController {
         currentViewController?.willMove(toParent: nil)
         currentViewController?.view.removeFromSuperview()
         currentViewController?.removeFromParent()
-
+        
         // 새 뷰 컨트롤러 추가
         addChild(newViewController)
         view.insertSubview(newViewController.view, belowSubview: tabBarView) // 탭바 아래에 추가
@@ -67,7 +67,7 @@ class CustomTabBarController: UIViewController {
             $0.edges.equalToSuperview() // 전체 화면에 꽉 차도록 설정
         }
         newViewController.didMove(toParent: self)
-
+        
         currentViewController = newViewController // 현재 뷰 컨트롤러 업데이트
     }
 }
